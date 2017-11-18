320c7768ddd0173ecf64425a329936af02a10da7
Szymon Sędek <szymon.sedek@gmail.com>
2017-11-18 09:48:11 +0100

    Dodanie Modeli

---
+++ Fuel/Fuel/ViewModel/FuelViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Fuel.Models;
+
+namespace Fuel.ViewModel
+{
+    public class FuelViewModel
+    {
+        public FuelModel FuelModel { get; set; }
+        public bool ShowResult { get; set; }
+    }
+}
